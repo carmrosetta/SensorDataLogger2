545a656c59995ff3e1dc2a0e3b084140c0b52640
Carmela Rosetta <carmela.rosetta@gmail.com>
2014-09-30 11:54:17 +0200

    second commit

--- app/src/main/java/ing/unipi/it/sensordatalogger2/MainActivity.java
+++ app/src/main/java/ing/unipi/it/sensordatalogger2/MainActivity.java
@@ -2,11 +2,13 @@ package ing.unipi.it.sensordatalogger2;
 
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
-import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.AdapterView;
@@ -18,9 +20,8 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import java.util.List;
-
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 
@@ -53,6 +54,7 @@ public class MainActivity extends ActionBarActivity {
     LinearLayout[] sensorChoices;
     int sensorTypes[];
     ArrayAdapter<CharSequence> [] samplingSpeedArrayAdapter;
+    int sensorDelays[];
 
 
 
@@ -79,8 +81,11 @@ public class MainActivity extends ActionBarActivity {
         sensorTypes = new int[sensorList.size()];
 
         samplingSpeedArrayAdapter = new ArrayAdapter[sensorList.size()];
+        sensorDelays = new int[sensorList.size()];
 
         for(int i = 0; i < sensorList.size(); i++) {
+
+            final int j = i;
 
             samplingSpeedArrayAdapter[i] = ArrayAdapter.createFromResource(this, R.array.delay_rates_array, android.R.layout.simple_spinner_item);
             samplingSpeedArrayAdapter[i].setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -99,6 +104,35 @@ public class MainActivity extends ActionBarActivity {
             sensorChoices[i].addView(sensorSamplingSpeeds[i]);
             sensorSamplingSpeeds[i].setLayoutParams(new LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT));
             sensorSamplingSpeeds[i].setAdapter(samplingSpeedArrayAdapter[i]);
+            sensorSamplingSpeeds[i].setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    switch (position) {
+                        case 0:
+                            sensorDelays[j] = SensorManager.SENSOR_DELAY_NORMAL;
+                            break;
+                        case 1:
+                            sensorDelays[j] = SensorManager.SENSOR_DELAY_UI;
+                            break;
+                        case 2:
+                            sensorDelays[j] = SensorManager.SENSOR_DELAY_GAME;
+                            break;
+                        case 3:
+                            sensorDelays[j] = SensorManager.SENSOR_DELAY_FASTEST;
+                        default:
+                            sensorDelays[j] = SensorManager.SENSOR_DELAY_NORMAL;
+                            break;
+                    }
+
+                    Toast.makeText(getApplicationContext(), "Sensor "+j+" speed = "+sensorDelays[j], Toast.LENGTH_LONG).show();
+
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
 
 
             sensorActivationList.addView(sensorChoices[i]);
@@ -137,6 +171,40 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+
+
+
+    public void startSampling(View v) {
+
+        int selectedId = radioSexGroup.getCheckedRadioButtonId();
+        radioSexButton = (RadioButton) findViewById(selectedId);
+        sex = radioSexButton.getText().toString();
+
+        age = ageEdit.getText().toString();
+
+        height = heightEdit.getText().toString();
+
+        weight = weightEdit.getText().toString();
+
+
+        //TODO controlli sull'obbligatorietà dei campi
+//        if ((sex.equals("")) || (age.equals("")) || (height.equals("")) || (weight.equals("")) || (sensorPosition.equals("")) ) {
+//
+//            tvErrors.setText("All fields are required!");
+//            tvErrors.setTextColor(Color.RED);
+//
+//        } else {
+//            Intent intent = new Intent(this.getApplicationContext(), SensorsSamplingService.class);
+//            User user = new User(sex, age, height, weight);
+//            intent.putExtra("User data", user);
+//            intent.putExtra("Sensor sampling rate", String.valueOf(sensorDelay));
+//            intent.putExtra("Sensor position", sensorPosition);
+//
+//            Log.d("Data sent to service", "From main activity to service");
+//            startService(intent);
+//            finish();
+//        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
